914c9880993039b4a2d49bcdefa37ea76269fa4b
oleg <oleg.kornelyuk@groslab.com>
2017-02-28 19:33:17 +0200

    fix build

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,9 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('static-v2').then(function(cache) {
+        caches.open('static-v3').then(function(cache) {
             return cache.addAll([
                 '/',
-                '/index.js',
-                '/static/js/bundle.js',
+                '/static/js/main.js',
                 '//cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.3/jspdf.debug.js'
             ]);
         })
@@ -12,7 +11,6 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
