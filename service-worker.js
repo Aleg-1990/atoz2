990d40560367e2009a05987129f5dfd4db66bcb3
oleg <oleg.kornelyuk@groslab.com>
2017-03-03 16:25:05 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,15 @@
 self.addEventListener('install', function(event) {
     const CACHE_VERSION = 8;
     event.waitUntil(
-        caches.open('static-'+CACHE_VERSION).then(function(cache) {
-            for (i = 0; i < CACHE_VERSION; i++) {
+        caches.open('static-v'+CACHE_VERSION).then(function(cache) {
+            for (i = 1; i < CACHE_VERSION; i++) {
                 caches.delete('static-v'+i);
             }
             return cache.addAll([
                 '/atoz2',
                 '/atoz2/static/js/main.js',
                 '//cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.3/jspdf.debug.js'
-            ]).then(() => {
-                for (i = 0; i < CACHE_VERSION; i++) {
-                    caches.delete('static-v'+i);
-                }
-            });
+            ]);
         })
     );
 });
